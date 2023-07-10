9d4a610502313450662cab2b0fa0d8b1437de972
Md Parvez <mdp020479@gmail.com>
2023-07-10 14:44:27 +0600

    new page and feature added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 import imageComponents from "@astrojs/image";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), imageComponents(), compress(), sitemap(), prefetch(), react()],
+  integrations: [
+    tailwind(),
+    imageComponents(),
+    compress(),
+    sitemap(),
+    prefetch(),
+    react(),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  site: "https://10mblogs.xyz",
 });
