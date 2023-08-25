6bf41498222126699afeb54072cdaff03a0d78cd
Md Parvez <mdp020479@gmail.com>
2023-08-25 12:26:33 +0600

    description added and astro-compress added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import imageComponents from "@astrojs/image";
-import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     imageComponents(),
-    compress(),
     sitemap(),
     prefetch(),
     react(),
+    compress(),
   ],
   output: "server",
   adapter: vercel(),
